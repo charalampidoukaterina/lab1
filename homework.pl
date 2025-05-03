97e40845b95c7581f4fe68a7939e5d39944bf6c0
charalampidoukaterina <aicharalamp@uth.gr>
2025-05-03 18:01:17 +0300

    upload

---
+++ homework.pl
@@ -0,0 +1,11 @@
+
+my $dna = "ATGCTAGC";
+
+my $length = length($dna);
+print "Length : $length\n";
+
+my $lowercase = lc($dna);
+print "Lowercase : $lowercase\n";
+
+my $reversed = reverse($dna);
+print "Reversed : $reversed\n";
